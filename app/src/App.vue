af18966ea9cd2c626cc0b83f2972a3b9f17d6626
IanL73 <ianl73@nycstudents.net>
2026-05-07 13:55:02 -0400

    push

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,85 +1,40 @@
 <script setup lang="ts">
+VITE_SUPABASE_URL=https://fawktaffalkeemtdkoqp.supabase.co
+VITE_SUPABASE_PUBLISHABLE_KEY=sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+import { ref, onMounted } from 'vue'
+import { supabase } from './supabase'
+
+const transactions = ref([])
+const error = ref(null)
+
+onMounted(async () => {
+  let { data: transactionData, error: err } = await supabase
+    .from('Fish')
+    .select('*')
+  if (err) {
+    error.value = err.message
+  } else {
+    transactions.value = transactionData
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<ul v-else>
+  <li v-for="transaction in transactions" :key="transaction.id">
+    ID: {{ transaction.id }} | User: {{ transaction.user_id }} | ...
+  </li>
+</ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style>
 </style>
