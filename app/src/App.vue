505f4b3a6c690577ce147fb7c63f4b7e414c9116
IanL73 <ianl73@nycstudents.net>
2026-05-19 13:33:58 -0400

    hmm

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,20 @@
+<template>
+<ul v-if="error">
+  <h2>
+    error!
+  </h2>
+<ul>
+<ul v-else>
+  <li v-for="transaction in transactions" :key="transaction.id">
+    ID: {{ transaction.id }} | User: {{ transaction.user_id }} | ...
+  </li>
+</ul>
+</template>
+
 <script setup lang="js">
 VITE_SUPABASE_URL="https://fawktaffalkeemtdkoqp.supabase.co"
 VITE_SUPABASE_PUBLISHABLE_KEY="sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa"
-import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -27,14 +35,6 @@ onMounted(async () => {
   }
 })
 </script>
-
-<template>
-<ul v-else>
-  <li v-for="transaction in transactions" :key="transaction.id">
-    ID: {{ transaction.id }} | User: {{ transaction.user_id }} | ...
-  </li>
-</ul>
-</template>
 
 <style>
 </style>
