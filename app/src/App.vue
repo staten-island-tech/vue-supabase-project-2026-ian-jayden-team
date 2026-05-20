3d88315322248e7910d2f6d689a8d3af3d58cfec
IanL73 <ianl73@nycstudents.net>
2026-05-20 14:01:05 -0400

    I *think* I'm making progress maybe

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,10 +8,29 @@ const error = ref(null)
 onMounted(async () => {
   let { data: fishData, error : err } = await supabase
     .from('Fish')
+    .select('*')
+  if (err) {
+    error.value = err.message
+    console.log("error")
+  } else {
+    fish.value = fishData
+    console.log("this is what is in the fish array")
+    console.log(fishData)
+    console.log(fish.value)
+  }
 })
-
 </script>
 
 <template>
-  
+
+<ul v-if="error">
+  <h1>error</h1>
+</ul>
+
+<ul v-else>
+  <li v-for="fishy in fish" :key="fish.id">
+    ID: {{ fishy.id }} | User: {{ fishy.fish_name }} | ...
+  </li>
+</ul>
+
 </template>
