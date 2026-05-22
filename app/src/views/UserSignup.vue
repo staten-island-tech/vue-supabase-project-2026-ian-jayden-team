2a774971f5a678cd475bfa7964d4ff49ba5e9a2b
IanL73 <ianl73@nycstudents.net>
2026-05-22 14:00:28 -0400

    g

---
+++ app/src/views/UserSignup.vue
@@ -0,0 +1,37 @@
+<script setup >
+  import { ref, onMounted } from 'vue'
+  import { supabase } from '../utils/supabase'
+  
+const fish = ref([])
+const error = ref(null)
+
+onMounted(async () => {
+  let { data: fishdata, error: err } = await supabase
+    .from('Fish')
+    .select('id, fish_name')
+  if (err) {
+    error.value = err.message
+  } else {
+    fish.value = fishdata
+    console.log(fish.value)
+  }
+})
+</script>
+
+<template>
+
+<form action="" method="get" class="form-example">
+  <div class="form-example">
+    <label for="name">Enter your name: </label>
+    <input type="text" name="name" id="name" required />
+  </div>
+  <div class="form-example">
+    <label for="email">Enter your email: </label>
+    <input type="email" name="email" id="email" required />
+  </div>
+  <div class="form-example">
+    <input type="submit" value="Subscribe!" />
+  </div>
+</form>
+
+</template>
