91482a8ce3156ae399cc6d1007cfcd53ec2c2a69
IanL73 <ianl73@nycstudents.net>
2026-06-02 14:02:54 -0400

    [p

--- app/src/views/SignUp.vue
+++ app/src/views/SignUp.vue
@@ -5,57 +5,26 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient('https://Fish.supabase.co', 'sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa')
 
+
+async function submit() {
+  let { data, error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,
+})
+
+}
 let username = ref('')
 let email = ref('')
 let password = ref('')
-
-function handleSubmit(e: Event) {
-  e.preventDefault()
-  let signUpInfo = ({ username: username.value, email: email.value, password: password.value })
-  console.log(signUpInfo)
-  console.log(username.value, email.value, password.value)
-
-}
-
-// Insert one row
-const { data, error } = await supabase
-  .from('your_table')
-  .insert({ column1: 'value', column2: 123 })
-  .select()
-
-if (error) console.error(error)
-else console.log(data)
-
-// Insert multiple rows
-const { data: rows, error: err } = await supabase
-  .from('your_table')
-  .insert([
-    { column1: 'a', column2: 1 },
-    { column1: 'b', column2: 2 },
-  ])
-  .select()
 </script>
 
 <template>
   <h1>Sign Up</h1>
-
-  <!-- use @submit.prevent to stop full page reload -->
-  <form id="myForm" @submit="handleSubmit">
-    <label>
-      Username:
-      <input type="text" name="name" v-model="username" required />
-    </label>
-
-    <label>
-      Email:
-      <input type="email" name="email" v-model="email" required />
-    </label>
-
-    <label>
-      Password:
-      <input type="password" name="password" v-model="password" required />
-    </label>
-
-    <button type="submit">Submit</button>
-  </form>
+  <p>input username</p>
+  <input v-model="username">
+  <p>input email</p>
+  <input v-model="email">
+  <p>input password</p>
+  <input v-model="password">
+  <button @click="submit">Sign Up</button>
 </template>
