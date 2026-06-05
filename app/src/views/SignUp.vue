396696c906cf1a96d5495923873abf990e468fc1
IanL73 <ianl73@nycstudents.net>
2026-06-05 14:01:39 -0400

    hhhhhh

--- app/src/views/SignUp.vue
+++ app/src/views/SignUp.vue
@@ -1,18 +1,30 @@
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue'
 import { supabase } from '../utils/supabase'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('https://Fish.supabase.co', 'sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa')
+const supabaseData = createClient('https://fawktaffalkeemtdkoqp.supabase.co', 'sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa')
 
+const users = ref([])
+const error = ref(null)
 
 async function submit() {
-  let { data, error } = await supabase.auth.signUp({
+  let { data, error } = await supabaseData.auth.signUp({
     email: email.value,
     password: password.value,
-})
-
+  })
+  let { data: userData, error: err } = await supabase
+    .from('users')
+    .select('id, email')
+  if (err) {
+    error.value = err.message
+  } else {
+    users.value = userData
+    console.log(users.value)
+  }
+  const user = 
 }
+
 let username = ref('')
 let email = ref('')
 let password = ref('')
@@ -27,4 +39,5 @@ let password = ref('')
   <p>input password</p>
   <input v-model="password">
   <button @click="submit">Sign Up</button>
+  <button @click="giveUserdata">give userdata</button>
 </template>
