2ce938cd3481b73955a5834418ac7c602fb6a5a2
JaydenZheng447 <jaydenzheng447@gmail.com>
2026-06-06 22:09:38 -0400

    Revert "Update SignUp.vue"
    This reverts commit c76bfd37f8ac11b82103da3d8410dd4bffccf8b0.

--- app/src/views/SignUp.vue
+++ app/src/views/SignUp.vue
@@ -5,34 +5,16 @@
 const fish = ref([])
 const error = ref("")
 
-<<<<<<< Updated upstream
 onMounted(async () => {
   let { data: fishdata, error: err } = await supabase
     .from('Fish')
     .select('id, fish_name')
-=======
-const supabaseData = createClient(
-  'https://fawktaffalkeemtdkoqp.supabase.co',
-  'sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa',
-)
-
-const users = ref([])
-const error = ref(null)
-
-async function submit() {
-  let { data, error } = await supabaseData.auth.signUp({
-    email: email.value,
-    password: password.value,
-  })
-  let { data: userData, error: err } = await supabase.from('users').select('id, email')
->>>>>>> Stashed changes
   if (err) {
     error.value = err.message
   } else {
     fish.value = fishdata
     console.log(fish.value)
   }
-<<<<<<< Updated upstream
 })
 </script>
 
@@ -53,24 +35,3 @@ async function submit() {
 </form>
 <button type="button">Add to favorites</button>
 </template>
-=======
-  // const user =
-}
-
-let username = ref('')
-let email = ref('')
-let password = ref('')
-</script>
-
-<template>
-  <h1>Sign Up</h1>
-  <p>input username</p>
-  <input v-model="username" />
-  <p>input email</p>
-  <input v-model="email" />
-  <p>input password</p>
-  <input v-model="password" />
-  <button @click="submit">Sign Up</button>
-  <button @click="giveUserdata">give userdata</button>
-</template>
->>>>>>> Stashed changes
