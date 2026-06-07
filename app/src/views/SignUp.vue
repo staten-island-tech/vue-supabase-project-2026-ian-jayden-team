b881c7bf228c078b1ddc45a7a55993a47e0acf7d
JaydenZheng447 <jaydenzheng447@gmail.com>
2026-06-06 22:12:02 -0400

    apparently when you switch branches on github VSCode will automatically change the text files to match it

--- app/src/views/SignUp.vue
+++ app/src/views/SignUp.vue
@@ -1,37 +1,44 @@
-<script setup lang="ts">
-  import { ref, onMounted } from 'vue'
-  import { supabase } from '../utils/supabase'
-  
-const fish = ref([])
-const error = ref("")
+<script setup>
+import { ref } from 'vue'
+import { supabase } from '../utils/supabase'
+import { createClient } from '@supabase/supabase-js'
 
-onMounted(async () => {
-  let { data: fishdata, error: err } = await supabase
-    .from('Fish')
-    .select('id, fish_name')
+const supabaseData = createClient(
+  'https://fawktaffalkeemtdkoqp.supabase.co',
+  'sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa',
+)
+
+const users = ref([])
+const error = ref(null)
+
+async function submit() {
+  let { data, error } = await supabaseData.auth.signUp({
+    email: email.value,
+    password: password.value,
+  })
+  let { data: userData, error: err } = await supabase.from('users').select('id, email')
   if (err) {
     error.value = err.message
   } else {
-    fish.value = fishdata
-    console.log(fish.value)
+    users.value = userData
+    console.log(users.value)
   }
-})
+  // const user =
+}
+
+let username = ref('')
+let email = ref('')
+let password = ref('')
 </script>
 
 <template>
-
-<h1>grass</h1>
-
-<form id="myForm">
-    <label>
-      Username:
-      <input type="text" name="name" required />
-    </label>
-    <label>
-      Email:
-      <input type="email" name="email" required />
-    </label>
-    <button type="submit">Submit</button>
-</form>
-<button type="button">Add to favorites</button>
+  <h1>Sign Up</h1>
+  <p>input username</p>
+  <input v-model="username" />
+  <p>input email</p>
+  <input v-model="email" />
+  <p>input password</p>
+  <input v-model="password" />
+  <button @click="submit">Sign Up</button>
+  <button @click="giveUserdata">give userdata</button>
 </template>
