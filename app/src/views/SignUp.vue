f4cd78c8ea33527ce74f42ff4edbc45a418abe2e
IanL73 <ianl73@nycstudents.net>
2026-06-07 20:43:17 -0400

    Sign up now fills out all necessary fields in auth users table and users table

--- app/src/views/SignUp.vue
+++ app/src/views/SignUp.vue
@@ -3,16 +3,25 @@ import { ref } from 'vue'
 import { supabase } from '../utils/supabase'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseData = createClient('https://Fish.supabase.co', 'sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa')
+const supabaseData = createClient('https://fawktaffalkeemtdkoqp.supabase.co', 'sb_publishable_DIBLDRPI2-eAlv1slYRteQ_lpIcNQIa')
 
+const users = ref([])
+const errorr = ref(null)
 
 async function submit() {
   let { data, error } = await supabaseData.auth.signUp({
     email: email.value,
     password: password.value,
-})
-
+  })
+  console.log(email.value)
+  console.log(username.value)
+  console.log(password.value)
+  const {  } = await supabase
+    .from('users')
+    .update({ username: username.value })
+    .eq('email', email.value)
 }
+
 let username = ref('')
 let email = ref('')
 let password = ref('')
